d1f1c72d976c0c731a28aba13f8040deb13382d5
Helen Xiao <helen.xiao@duke.edu>
2020-10-03 23:45:08 -0400

    added header

---
+++ miniamazon-frontend/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home(){
+    return(
+        <div>
+            <h1>Mini-Amazon</h1>
+            <h5>Hello! Welcome to Mini-Amazon!</h5>
+        </div>
+    )
+}
+
+export default Home;
