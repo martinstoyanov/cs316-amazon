6dbb487715c3fdd7bdbdc277a50243b8266b5cd3
Daniela Trejo <daniela.trejo@duke.edu>
2020-10-18 17:03:23 -0400

    i think i connected front/back for cart. pls check me

--- miniamazon-frontend/src/components/Cart.js
+++ miniamazon-frontend/src/components/Cart.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import Product from './Product';
+import axios from 'axios';
 
-function Cart(){
+/*function Cart(){
     var isCheckedOut = false;
 
     var cartID = 123456;
@@ -29,75 +30,211 @@ function Cart(){
     var billingAddressCity = "Durham"
     var billingAddressState = "NC"
     var billingAddressZip = "27706"
+*/
+    
+    const serverURL = "http://localhost:8888"
+    export default class Cart extends React.Component{
+        constructor(props) {
+            super(props);
+            this.state = {
+              orderID: "",
+              orderName: "",
+              shippingAddressLine1: "",
+              shippingAddressLine2: "",
+              shippingAddressCity: "",
+              shippingAddressState: "",
+              shippingAddressZip: "",
+              paymentCardNumber: "",
+              paymentCardExpire: "",
+              paymentCardSecurityCode: "",
+              paymentCardName: "",
+              billingName: "",
+              billingAddressLine1: "",
+              billingAddressLine2: "",
+              billingAddressCity: "",
+              billingAddressState: "",
+              billingAddressZip: "",
+              orderPrice: 0.0,
+              orderStatus: "",
+              orderDate: "",
+              orderDeliveryDate: "",
+              orderDateCheckedOut: "",
+              orderPrice: false
+            };
+        }
+         setOrderID(event) {
+            this.state ({orderID: event.target.value})
+        }
+         setOrderName(event) {
+            this.state ({orderName: event.target.value})
+        }
+         setShippingAddressLine1(event) {
+            this.state ({shippingAddressLine1: event.target.value})
+        }
+        setShippingAddressLine2(event) {
+            this.state ({shippingAddressLine2: event.target.value})
+        }
+         setShippingAddressCity(event) {
+            this.state ({shippingAddressCity: event.target.value})
+        }
+         setShippingAddressState(event) {
+            this.state ({shippingAddressState: event.target.value})
+        }
+        setShippingAddressZip(event) {
+            this.state ({shippingAddressZip: event.target.value})
+        }
+        setPaymentCardNumber(event) {
+            this.state ({paymentCardNumber: event.target.value})
+        }
+        setPaymentCardExpire(event) {
+            this.state ({paymentCardExpire: event.target.value})
+        }
+        setPaymentCardSecurityCode(event) {
+            this.state ({paymentCardSecurityCode: event.target.value})
+        }
+        setPaymentCardName(event) {
+            this.state ({paymentCardName: event.target.value})
+        }
+        setBillingName(event) {
+            this.state ({billingName: event.target.value})
+        }
+        setBillingAddressLine1(event) {
+            this.state ({billingAddressLine1: event.target.value})
+        }
+        setBillingAddressLine2(event) {
+            this.state ({billingAddressLine2: event.target.value})
+        }
+        setBillingAddressCity(event) {
+            this.state ({billingAddressCity: event.target.value})
+        }
+        setBillingAddressState(event) {
+            this.state ({billingAddressState: event.target.value})
+        }
+        setBillingAddressZip(event) {
+            this.state ({billingAddressZip: event.target.value})
+        }
+        setOrderPrice(event) {
+            this.state ({orderPrice: event.target.value})
+        }
+        setOrderStatus(event) {
+            this.state ({orderStatus: event.target.value})
+        }
+        setOrderDate(event) {
+            this.state ({orderDate: event.target.value})
+        }
+        setOrderDeliveryDate(event) {
+            this.state ({orderDeliveryDate: event.target.value})
+        }
+        setOrderDateCheckedOut(event) {
+            this.state ({orderDateCheckedOut: event.target.value})
+        } 
+        render(){
+            return(
+                <nav className="left-layout">
+                    <h1>CART</h1>
+                    <h5>Below will list out your cart items.</h5>
+                    <div>
+                        {productList}
+                    </div>
+                    <div id="shipping">
+                        <h2>CHECKOUT:</h2>
+                        <table className="shipping-inputs">
+                            <tbody>
+                                <tr><td><p className = "label"><h3>Step 1: Shipping Address</h3></p></td></tr>
+                                <tr id="order-name">
+                                    <td><p className = "label">First and Last Name: </p></td>
+                                    <td><input type = "text" className = "set-order-name" onChange = {this.setOrderName}/></td>
+                                </tr>
+                                <tr id="shipping-address-line1">
+                                    <td><p className = "label">Address Line 1: </p></td>
+                                    <td><input type = "text" className = "set-shipping-address" onChange = {this.setShippingAddressLine1}/></td>
+                                </tr>
+                                <tr id="shipping-address-line2">
+                                    <td><p className = "label">Address Line 2: </p></td>
+                                    <td><input type = "text" className = "set-shipping-address" onChange = {this.setShippingAddressLine2}/></td>
+                                </tr>
+                                <tr id="shipping-address-city">
+                                    <td><p className = "label">City: </p></td>
+                                    <td><input type = "text" className = "set-shipping-address" onChange = {this.setShippingAddressCity}/></td>
+                                </tr>
+                                <tr id="shipping-address-state">
+                                    <td><p className = "label">State: </p></td>
+                                    <td><input type = "text" className = "set-shipping-address" onChange = {this.setShippingAddressState}/></td>
+                                </tr>
+                                <tr id="shipping-address-zip">
+                                    <td><p className = "label">Zip Code: </p></td>
+                                    <td><input type = "text" className = "set-shipping-address" onChange = {this.setShippingAddressZip}/></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div><br/>
+        
+                    <div id="payment">
+                        <table className="payment-inputs">
+                            <tbody>
+                                <tr><td><p className = "label"><h3>Step 2: Payment Method</h3></p></td></tr>
+                                <tr id="payment-card-number">
+                                    <td><p className = "label">Credit Card Number:</p></td>
+                                    <td><input type = "text" className = "set-payment" onChange = {this.setPaymentCardNumber}/></td>
+                                </tr>
+                                <tr id="payment-expire">
+                                    <td><p className = "label">Expiration Date (MM/YY):</p></td>
+                                    <td><input type = "text" className = "set-payment" onChange = {this.setPaymentExpire}/></td>
+                                </tr>
+                                <tr id="payment-security-code">
+                                    <td><p className = "label">Security Code:</p></td>
+                                    <td><input type = "text" className = "set-payment" onChange = {this.setPaymentSecurityCode}/></td>
+                                </tr>
+                                <tr id="payment-name">
+                                    <td><p className = "label">Name on Card:</p></td>
+                                    <td><input type = "text" className = "set-payment" onChange = {this.setPaymentName}/></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div><br/>
+        
+                    <div id="billing">
+                        <table className="billing-inputs">
+                            <tbody>
+                                <tr><td><p className = "label"><h3>Step 3: Billing Information</h3></p></td></tr>
+                                <tr id="billing-name">
+                                    <td><p className = "label">First and Last Name: </p></td>
+                                    <td><input type = "text" className = "set-billing-name" onChange = {this.setBillingName}/></td> ********
+                                </tr>
+                                <tr id="billing-address-line1">
+                                    <td><p className = "label">Address Line 1: </p></td>
+                                    <td><input type = "text" className = "set-billing-address" onChange = {this.setBillingAddressLine1}/></td>
+                                </tr>
+                                <tr id="billing-address-line2">
+                                    <td><p className = "label">Address Line 2: </p></td>
+                                    <td><input type = "text" className = "set-billing-address" onChange = {this.setBillingAddressLine2}/></td>
+                                </tr>
+                                <tr id="billing-address-city">
+                                    <td><p className = "label">City: </p></td>
+                                    <td><input type = "text" className = "set-billing-address" onChange = {this.setBillingAddressCity}/></td>
+                                </tr>
+                                <tr id="billing-address-state">
+                                    <td><p className = "label">State: </p></td>
+                                    <td><input type = "text" className = "set-billing-address" onChange = {this.setBillingAddressState}/></td>
+                                </tr>
+                                <tr id="billing-address-zip">
+                                    <td><p className = "label">Zip Code: </p></td>
+                                    <td><input type = "text" className = "set-billing-address" onChange = {this.setBillingAddressZip}/></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div><br/>
+                    <div>
+                        <a href="/thanks">
+                            <button className="btn btn-secondary" onClick={setCheckedOut}>CHECKOUT!</button><br/>
+                        </a>
+                    </div><br/><br/>
+                </nav>
+            )
+        }
+    
 
-    function setOrderID(event) {
-        orderID = event.target.value;
-    }
-    function setOrderName(event) {
-        orderName = event.target.value;
-    }
-    function setShippingAddressLine1(event) {
-        shippingAddressLine1 = event.target.value;
-    }
-    function setShippingAddressLine2(event) {
-        shippingAddressLine2 = event.target.value;
-    }
-    function setShippingAddressCity(event) {
-        shippingAddressCity = event.target.value;
-    }
-    function setShippingAddressState(event) {
-        shippingAddressState = event.target.value;
-    }
-    function setShippingAddressZip(event) {
-        shippingAddressZip = event.target.value;
-    }
-    function setPaymentCardNumber(event) {
-        paymentCardNumber = event.target.value;
-    }
-    function setPaymentExpire(event) {
-        paymentExpire = event.target.value;
-    }
-    function setPaymentSecurityCode(event) {
-        paymentSecurityCode = event.target.value;
-    }
-    function setPaymentName(event) {
-        paymentName = event.target.value;
-    }
-    function setBillingName(event) {
-        orderName = event.target.value;
-    }
-    function setBillingAddressLine1(event) {
-        shippingAddressLine1 = event.target.value;
-    }
-    function setBillingAddressLine2(event) {
-        shippingAddressLine2 = event.target.value;
-    }
-    function setBillingAddressCity(event) {
-        shippingAddressCity = event.target.value;
-    }
-    function setBillingAddressState(event) {
-        shippingAddressState = event.target.value;
-    }
-    function setBillingAddressZip(event) {
-        shippingAddressZip = event.target.value;
-    }
-    function setOrderPrice(event) {
-        orderPrice = event.target.value;
-    }
-    function setOrderStatus(event) {
-        orderStatus = event.target.value;
-    }
-    function setOrderDate(event) {
-        orderDate = event.target.value;
-    }
-    function setDeliveryDate(event) {
-        deliveryDate = event.target.value;
-    }
-    function setCheckedOut(event) {
-        isCheckedOut = true;
-    }
-
-    const products = [
+    /*const products = [
         {
             itemID: 1,
             itemName: "item 1",
@@ -115,110 +252,12 @@ function Cart(){
     ]
     const productList = products.map(product => 
         <Product product ={product}></Product>
-    )
+    )*/
 
-    return(
-        <nav className="left-layout">
-            <h1>CART</h1>
-            <h5>Below will list out your cart items.</h5>
-            <div>
-                {productList}
-            </div>
-            <div id="shipping">
-                <h2>CHECKOUT:</h2>
-                <table className="shipping-inputs">
-                    <tbody>
-                        <tr><td><p className = "label"><h3>Step 1: Shipping Address</h3></p></td></tr>
-                        <tr id="order-name">
-                            <td><p className = "label">First and Last Name: </p></td>
-                            <td><input type = "text" className = "set-order-name" onChange = {setOrderName}/></td>
-                        </tr>
-                        <tr id="shipping-address-line1">
-                            <td><p className = "label">Address Line 1: </p></td>
-                            <td><input type = "text" className = "set-shipping-address" onChange = {setShippingAddressLine1}/></td>
-                        </tr>
-                        <tr id="shipping-address-line2">
-                            <td><p className = "label">Address Line 2: </p></td>
-                            <td><input type = "text" className = "set-shipping-address" onChange = {setShippingAddressLine2}/></td>
-                        </tr>
-                        <tr id="shipping-address-city">
-                            <td><p className = "label">City: </p></td>
-                            <td><input type = "text" className = "set-shipping-address" onChange = {setShippingAddressCity}/></td>
-                        </tr>
-                        <tr id="shipping-address-state">
-                            <td><p className = "label">State: </p></td>
-                            <td><input type = "text" className = "set-shipping-address" onChange = {setShippingAddressState}/></td>
-                        </tr>
-                        <tr id="shipping-address-zip">
-                            <td><p className = "label">Zip Code: </p></td>
-                            <td><input type = "text" className = "set-shipping-address" onChange = {setShippingAddressZip}/></td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div><br/>
+    
+    componentDidMount() {
+        axios.get(`${serverURL}/items`).then((response) => {
+            this.setState({products: response.data.data})
+        });
+    }}
 
-            <div id="payment">
-                <table className="payment-inputs">
-                    <tbody>
-                        <tr><td><p className = "label"><h3>Step 2: Payment Method</h3></p></td></tr>
-                        <tr id="payment-card-number">
-                            <td><p className = "label">Credit Card Number:</p></td>
-                            <td><input type = "text" className = "set-payment" onChange = {setPaymentCardNumber}/></td>
-                        </tr>
-                        <tr id="payment-expire">
-                            <td><p className = "label">Expiration Date (MM/YY):</p></td>
-                            <td><input type = "text" className = "set-payment" onChange = {setPaymentExpire}/></td>
-                        </tr>
-                        <tr id="payment-security-code">
-                            <td><p className = "label">Security Code:</p></td>
-                            <td><input type = "text" className = "set-payment" onChange = {setPaymentSecurityCode}/></td>
-                        </tr>
-                        <tr id="payment-name">
-                            <td><p className = "label">Name on Card:</p></td>
-                            <td><input type = "text" className = "set-payment" onChange = {setPaymentName}/></td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div><br/>
-
-            <div id="billing">
-                <table className="billing-inputs">
-                    <tbody>
-                        <tr><td><p className = "label"><h3>Step 3: Billing Information</h3></p></td></tr>
-                        <tr id="billing-name">
-                            <td><p className = "label">First and Last Name: </p></td>
-                            <td><input type = "text" className = "set-billing-name" onChange = {setBillingName}/></td>
-                        </tr>
-                        <tr id="billing-address-line1">
-                            <td><p className = "label">Address Line 1: </p></td>
-                            <td><input type = "text" className = "set-billing-address" onChange = {setBillingAddressLine1}/></td>
-                        </tr>
-                        <tr id="billing-address-line2">
-                            <td><p className = "label">Address Line 2: </p></td>
-                            <td><input type = "text" className = "set-billing-address" onChange = {setBillingAddressLine2}/></td>
-                        </tr>
-                        <tr id="billing-address-city">
-                            <td><p className = "label">City: </p></td>
-                            <td><input type = "text" className = "set-billing-address" onChange = {setBillingAddressCity}/></td>
-                        </tr>
-                        <tr id="billing-address-state">
-                            <td><p className = "label">State: </p></td>
-                            <td><input type = "text" className = "set-billing-address" onChange = {setBillingAddressState}/></td>
-                        </tr>
-                        <tr id="billing-address-zip">
-                            <td><p className = "label">Zip Code: </p></td>
-                            <td><input type = "text" className = "set-billing-address" onChange = {setBillingAddressZip}/></td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div><br/>
-            <div>
-                <a href="/thanks">
-                    <button className="btn btn-secondary" onClick={setCheckedOut}>CHECKOUT!</button><br/>
-                </a>
-            </div><br/><br/>
-        </nav>
-    )
-}
-
-export default Cart;
