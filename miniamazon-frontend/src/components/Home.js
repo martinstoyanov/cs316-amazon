2ef5815547b3328d8331f0742e080e30f48ba992
Helen Xiao <helen.xiao@duke.edu>
2020-11-12 00:50:02 -0500

    more errores

--- miniamazon-frontend/src/components/Home.js
+++ miniamazon-frontend/src/components/Home.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import axios from 'axios';
+
+const serverURL = "http://localhost:8888"
 
 export default class Home extends React.Component{
 
     constructor(props) {
         super(props);
+        this.state = {
+            categories:[]
+        }
+    }
+    
+    componentDidMount() {
+        //first, retrieve item object from database
+        axios.get(`${serverURL}/categories`).then((response) => {
+            // this.setState({item: response.data.data});
+            // let reviews = []
+            // let sumRating = 0.0
+            // let numRatings = 0.0
+
+            // //for each review in item.reviews, retrieve review object from database and append to this.state.reviews for display
+            // response.data.data.reviews.forEach(review_id => {
+            //     axios.get(`${serverURL}/review/${review_id}`).then((r) => {
+            //         let review = r.data.data
+            //         reviews.push(review)
+            //         sumRating += review.review_rating
+            //         numRatings = numRatings + 1 
+            //         this.setState({ reviews: reviews, avgReview: (sumRating / numRatings) })
+            //     });
+            // });
+        });
     }
 
     render() {
